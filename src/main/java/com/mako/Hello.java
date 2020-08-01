6f069881a9e736f2f2e476dbee63f6ecdc90e1c1
chw790404@gmail.com <makocho@systex.com>
2020-08-02 00:36:24 +0800

    Java and Kotlin variable, data type

--- src/main/java/com/mako/Hello.java
+++ src/main/java/com/mako/Hello.java
@@ -5,5 +5,15 @@ public class Hello {
 //        System.out.println("Hello World");
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2 = 20;
+        char c = '我';
+        Character c2 = 'A';
+        byte b = 120;
+        float weight = 43.4f;
+        float height = 1.4f;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Mako";
     }
 }
--- src/main/kotlin/com/mako/Hello.kt
+++ src/main/kotlin/com/mako/Hello.kt
@@ -5,6 +5,14 @@ fun main() {
 //    Human().hello()
     val h = Human()
     h.hello()
+    var age = 21
+    age = 24
+    var weight = 60.3
+    var height = 172.2f
+
+    var name : String;
+    name = "Mako"
+
 }
 
 class Human {
